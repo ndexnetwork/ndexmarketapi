a020106cac890e21d750f203dfc8933ff9c3358d
Tanvir Rahaman <40170544+ndexvictoria@users.noreply.github.com>
2018-12-26 06:14:45 +0400

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,19 +1,4 @@
-# ForkDelta Backend
-# https://github.com/forkdelta/backend-replacement
-# Copyright (C) 2018, Arseniy Ivanov and ForkDelta Contributors
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 
 # Eternally constant constants
@@ -21,5 +6,6 @@
 ZERO_ADDR = "0x0000000000000000000000000000000000000000"
 ZERO_ADDR_BYTES = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
-# We might change our mind about these:
+
 FILTER_ORDERS_UNDER_ETH = 0.001
+MAX_ORDERS_PER_USER = 8
