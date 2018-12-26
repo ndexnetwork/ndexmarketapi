828844427c7620be882b4bc718eff570fd5557ea
Tanvir Rahaman <40170544+ndexvictoria@users.noreply.github.com>
2018-12-26 06:02:11 +0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,19 +1,4 @@
-# ForkDelta Backend
-# https://github.com/forkdelta/backend-replacement
-# Copyright (C) 2018, Arseniy Ivanov and ForkDelta Contributors
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 from os import environ
 
@@ -28,7 +13,7 @@ with open('etherdelta.abi.json') as f:
     import json
     ED_CONTRACT_ABI = json.load(f)
 ED_WS_SERVERS = [
-    "wss://api.ndexnetwork.com/socket.io/?EIO=3&transport=websocket"
+    "wss://api.ndexnetwork.com/socket.io/?EIO=3&transport=websocket",
 ]
 
 POSTGRES_HOST = "postgres"
